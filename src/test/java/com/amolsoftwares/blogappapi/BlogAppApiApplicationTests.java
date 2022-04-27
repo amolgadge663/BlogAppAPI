dd9fd9c3fdf81d1a90253d31bc94c4d7d0ae5529
Amol <amolgadge663@gmail.com>
2022-04-27 09:45:07 +0530

    Initial Commit

---
+++ src/test/java/com/amolsoftwares/blogappapi/BlogAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amolsoftwares.blogappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

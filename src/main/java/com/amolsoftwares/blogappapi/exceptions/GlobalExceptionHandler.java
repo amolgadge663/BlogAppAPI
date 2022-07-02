dcab52aa4929e8b7ac80bada051c1461dcc0e562
Amol <amolgadge663@gmail.com>
2022-07-02 18:55:19 +0530

    User API complete

--- src/main/java/com/amolsoftwares/blogappapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/amolsoftwares/blogappapi/exceptions/GlobalExceptionHandler.java
@@ -3,17 +3,35 @@ package com.amolsoftwares.blogappapi.exceptions;
 import com.amolsoftwares.blogappapi.payload.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    ResponseEntity<ApiResponse> resourceNotFoundException(ResourceNotFoundException ex){
+    public ResponseEntity<ApiResponse> resourceNotFoundException(ResourceNotFoundException ex){
         String msg = ex.getMessage();
         ApiResponse apiResponse = new ApiResponse(msg, false);
         return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handlerMethodArgsNotValidException(MethodArgumentNotValidException ex){
+
+        Map<String, String> resp = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String defaultMessage = error.getDefaultMessage();
+            resp.put(fieldName, defaultMessage);
+        });
+
+        return new ResponseEntity<Map<String, String>>(resp, HttpStatus.BAD_REQUEST);
     }
 
 }
--- src/main/java/com/amolsoftwares/blogappapi/payload/UserDto.java
+++ src/main/java/com/amolsoftwares/blogappapi/payload/UserDto.java
@@ -4,15 +4,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @NoArgsConstructor
 @Getter
 @Setter
 public class UserDto {
 
     private int id;
+    @NotEmpty
+    @Size(min = 4, message = "User Name must be min of 4 characters!!!")
     private String name;
+    @Email(message = "Email address is not valid!!!")
     private String email;
+    @NotEmpty
+    @Size(min = 3, max = 10, message = "Password should be min 3 and max 10 characters!!!")
     private String password;
+    @NotEmpty
     private String about;
 
 }
